e64f9782221dcd571e02fc5c62b4f7abbe4e4aae
sobee-dev <obinnasamueledeh10000@gmail.com>
2025-10-04 18:57:08 +0100

    finally created a proper migrations

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 
 from flask import Flask
 
-
-from models.database import User
 from views.controller import controller
 from config.config import Config
 from extensions import db, mail, login_manager
@@ -17,8 +15,14 @@ app.config.from_object(Config)
 # Initialize extensions
 db.init_app(app)
 mail.init_app(app)
+
+from models.database import User,Interests,Admin
 migrate = Migrate(app, db)
+# print('migration successful')
 login_manager.init_app(app)
+
+
+
 # ensure upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
@@ -28,6 +32,7 @@ def inject_year():
     return {"year": datetime.now().year}
 
 app.register_blueprint(controller)
+
 if __name__ == "__main__":
     with app.app_context():
         # This will only run when you actually start the app
